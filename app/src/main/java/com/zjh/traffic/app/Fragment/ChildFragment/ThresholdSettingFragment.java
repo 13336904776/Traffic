94f26360671cfe10a8a6ac3026c5743fc833d5b0
zoujunhao <728600405@qq.com>
2018-11-08 17:07:40 +0800

    更新

--- app/src/main/java/com/zjh/traffic/app/Fragment/ChildFragment/ThresholdSettingFragment.java
+++ app/src/main/java/com/zjh/traffic/app/Fragment/ChildFragment/ThresholdSettingFragment.java
@@ -1,5 +1,6 @@
 package com.zjh.traffic.app.Fragment.ChildFragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,10 +8,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.zjh.traffic.R;
+import com.zjh.traffic.app.Application.App;
 
 public class ThresholdSettingFragment extends Fragment {
+    private TextView threshold_tv;
+    private EditText threshold_ed;
+    private Button btn_thresholdsetting;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -20,5 +29,22 @@ public class ThresholdSettingFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initView(view);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void initView(View view) {
+        threshold_tv = view.findViewById(R.id.threshold_tv);
+        threshold_ed = view.findViewById(R.id.threshold_ed);
+        btn_thresholdsetting = view.findViewById(R.id.btn_thresholdsetting);
+        threshold_tv.setText(App.getAlerting() + "");
+        btn_thresholdsetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                App.setAlerting(Integer.parseInt(threshold_ed.getText().toString()));
+                App.showAlertDialog(getContext(), "提醒", "设置成功", null);
+                threshold_tv.setText(App.getAlerting() + "");
+            }
+        });
     }
 }
