90480a3ee5eef8e7980882447a7ba9de235f16f3
zoujunhao <728600405@qq.com>
2018-11-20 23:19:46 +0800

    更新

--- app/src/main/java/com/zjh/traffic/app/Fragment/LifeAssistantFragment.java
+++ app/src/main/java/com/zjh/traffic/app/Fragment/LifeAssistantFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.zjh.traffic.R;
 
@@ -14,6 +16,11 @@ import com.zjh.traffic.R;
  * 生活助手
  */
 public class LifeAssistantFragment extends Fragment {
+    private TextView temperature_now, temperature_today, UVI_intensity, UVI_data,
+            cold_intensity, cold_data, dress_intensity, dress_data, sports_intensity,
+            sports_data, air_intensity, air_data;
+    private ImageButton btn_refresh;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,5 +34,18 @@ public class LifeAssistantFragment extends Fragment {
     }
 
     private void initView(View view) {
+        temperature_now = view.findViewById(R.id.temperature_now);
+        temperature_today = view.findViewById(R.id.temperature_today);
+        UVI_intensity = view.findViewById(R.id.UVI_intensity);
+        UVI_data = view.findViewById(R.id.UVI_data);
+        cold_intensity = view.findViewById(R.id.cold_intensity);
+        cold_data = view.findViewById(R.id.cold_data);
+        dress_intensity = view.findViewById(R.id.dress_intensity);
+        dress_data = view.findViewById(R.id.dress_data);
+        sports_intensity = view.findViewById(R.id.sports_intensity);
+        sports_data = view.findViewById(R.id.sports_data);
+        air_intensity = view.findViewById(R.id.air_intensity);
+        air_data = view.findViewById(R.id.air_data);
+        btn_refresh = view.findViewById(R.id.btn_refresh);
     }
 }
